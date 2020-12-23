29ac68f5f115fa31db34074944613c969dbf2cb4
Fraanlol <franco.catas@hotmail.com>
2020-12-23 20:40:12 -0300

    Finished adding all the files and the first working prototype

--- game.js
+++ game.js
@@ -1,22 +1,29 @@
 let playerScore = 0;
 let computerScore = 0;
-let options = ["rock", "paper","scissors"];
+let options = ["fire", "watter","grass"];
 let textIndex = 0;
 let titleText = 'Be prepared to defend yourself against \"Daffy Duck, The Wizard\" or your secrets will be leaked.';
-let instructionText = 'You\'ll have to defeat Master Daffy Duck in a ROCK-PAPER-SCISSORS! match. The rules are simple, but magic aren\'t so here are the instructions.';
+let instructionText = 'You\'ll have to defeat Master Daffy Duck in a FIRE-GRASS-WATTER! match. The rules are simple, but magic aren\'t, so here are the instructions.';
 const textSpeed = 70;
 let startButton = document.querySelector('.startButton');
 startButton.addEventListener('click', startGame);
 let introductText = document.querySelector('.insertedText');
 let instructionBtt= document.querySelector('.instruction');
 instructionBtt.addEventListener('click', showInstructions);
+let battleWeapons = document.querySelectorAll('.attack');
+let pointsUser = document.querySelector('.counterPlayer');
+let pointsMachine = document.querySelector('.counterMachine');
+let statusMessage = document.querySelector('.status');
+let restartButton = document.querySelector('.restartButtonActivable');
 
 window.onload = magicText = () => {
     if (textIndex < titleText.length) {
         introductText.innerHTML += titleText.charAt(textIndex);
         textIndex++;
-        setTimeout(magicText, textSpeed);
+        setTimeout(magicText, textSpeed/5);
     } else{
+        instructionBtt.style.display='block';
+        startButton.style.display='block';
         return textIndex = 0;
     }  
 };
@@ -26,14 +33,17 @@ function instructionList(){
         introductText.innerHTML += instructionText.charAt(textIndex);
         textIndex++;
         setTimeout(instructionList, textSpeed/2);
+    } else {
+        startButton.style.display='block';
     }
 }
 
 function showInstructions(){
-    startButton.style.display = 'none';
+    startButton.style.cssText = 'text-align:center; margin-top:20px;'
     instructionBtt.style.display = 'none';
     introductText.innerHTML = " ";
     instructionList();
+    document.querySelector('.elements').style.display = 'initial';
 }
 
 
@@ -41,40 +51,83 @@ function startGame(){
     startButton.style.display = 'none';
     instructionBtt.style.display = 'none';
     introductText.style.display= 'none';
+    document.querySelector('.elements').style.display = 'none';
+    document.querySelector('.playButtons').style.display = 'initial';
+    document.querySelector('#pointsCounter').style.display= 'initial';
 }
 
 
 
+function computerSelection(){
+    return (options[Math.floor(Math.random() * Math.floor(3))]);
+}
 
+function winner(){
+    if (playerScore == 5 || computerScore == 5){
+        restartButton.style.display = 'inline-block';
+        replayGame();
+        battleWeapons.forEach((button) => {
+            button.setAttribute('disabled', '');
+        })
+        if (playerScore > computerScore){
+            statusMessage.innerHTML = 'You\'ve won'
+            statusMessage.style.cssText = 'animation-play-state: paused; color:green !important;';
+        } else {
+            statusMessage.innerHTML = 'You\'ve lost'
+            statusMessage.style.cssText = 'animation-play-state: paused; color:red !important;';
+        }  
+    }
+   
+    
+}
+function replayGame(){
+    
+    restartButton.addEventListener('click',() =>{
+        window.location.reload();
+    })
+}
+function battleStart(playerSelection, computerPlay){
+    let playerChoice = document.querySelector('.userChoice');
+    let computerChoice = document.querySelector('.computerChoice');
+    let roundWin = document.querySelector('.roundWinner');
+    if (playerSelection === computerPlay){
+                playerChoice.innerHTML = `You\'ve chosen ${playerSelection} against`
+                computerChoice.innerHTML = `${computerPlay} spell`
+                roundWin.innerHTML = `That means a draw, you\'re both good magicians`
+            }
+            else if (playerSelection === options[0] && computerPlay !== options[1] || playerSelection === options[1] && computerPlay !== options[2] || playerSelection === options[2] && computerPlay !== options[0]){
+                playerChoice.innerHTML = `Your ${playerSelection} spell were SO effective against`
+                computerChoice.innerHTML = `Duffy's ${computerPlay} spell!`
+                roundWin.innerHTML = `You've won 1 point!`
+                playerScore+=1;
+                pointsUser.innerHTML = playerScore;
+            }
+            else{ 
+                playerChoice.innerHTML = `Bad choice wizard, ${playerSelection} spell missed against`
+                computerChoice.innerHTML = `the powerfull ${computerPlay} of the Mighty Duffy`
+                roundWin.innerHTML = `Duffy wons 1 point!`
+            computerScore+=1;
+            pointsMachine.innerHTML = computerScore;
+            }
+        console.log(playerScore);
+        console.log(computerScore);
+}
+function selections(){
+    let playerSelection;
 
-
-
-
-
-
-
-// function computerSelection(){
-//     return (options[Math.floor(Math.random() * Math.floor(3))]);
-// }
-
-//     function game () {
-//         console.log("Juego Iniciado");
-//         const playerSelection = prompt("What's your choice");
-//         let computerPlay = computerSelection();
-//         console.log(`You selected ${playerSelection}`);
-//         console.log(`Computed selected ${computerPlay}`);
-       
-//         if (playerSelection === computerPlay){
-//              console.log("TIE");
-//         }
-//         else if (playerSelection === options[0] && computerPlay !== options[1] || playerSelection === options[1] && computerPlay !== options[2] || playerSelection === options[2] && computerPlay !== options[0]){
-//             console.log(`You've won! ${playerSelection} beats ${computerPlay}`);
-//             playerScore+=1;
-//         }
-//         else{ 
-//         console.log(`Computer wins! ${computerPlay} beats ${playerSelection}`);
-//         computerScore+=1;
-//     }
-//     console.log(playerScore);
-//     console.log(computerScore);
-// }
+    battleWeapons.forEach((weapon) => {
+        weapon.addEventListener('click', () => {
+            if (weapon.classList.contains('fire_attack')){
+                playerSelection = options[0];
+            } else if ( weapon.classList.contains('water_attack')){
+                playerSelection = options[1];
+            } else {
+                playerSelection = options[2];
+            }
+            battleStart(playerSelection, computerSelection());
+            winner();
+        })
+    })
+   
+}
+selections();

a369278585f5c32b50fd8dd85985c821bcc09322
Fraanlol <franco.catas@hotmail.com>
2020-12-23 22:32:03 -0300

    Fixed Responsive and added some details

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
-let options = ["fire", "watter","grass"];
+let options = ["Fire", "Water","Grass"];
 let textIndex = 0;
 let titleText = 'Be prepared to defend yourself against \"Daffy Duck, The Wizard\" or your secrets will be leaked.';
 let instructionText = 'You\'ll have to defeat Master Daffy Duck in a FIRE-GRASS-WATTER! match. The rules are simple, but magic aren\'t, so here are the instructions.';
@@ -15,6 +15,7 @@ let pointsUser = document.querySelector('.counterPlayer');
 let pointsMachine = document.querySelector('.counterMachine');
 let statusMessage = document.querySelector('.status');
 let restartButton = document.querySelector('.restartButtonActivable');
+let buttonsContainers =  document.querySelectorAll('.containerButton')
 
 window.onload = magicText = () => {
     if (textIndex < titleText.length) {
@@ -22,37 +23,37 @@ window.onload = magicText = () => {
         textIndex++;
         setTimeout(magicText, textSpeed/5);
     } else{
-        instructionBtt.style.display='block';
-        startButton.style.display='block';
+        buttonsContainers.forEach((buttons) => {
+            buttons.style.display='block';
+        })
         return textIndex = 0;
     }  
 };
 
 function instructionList(){
+    document.querySelector('.containerInstruction').style.display='none';
+    document.querySelector('.containerStart').style.display='none';
     if (textIndex < instructionText.length) {
         introductText.innerHTML += instructionText.charAt(textIndex);
         textIndex++;
         setTimeout(instructionList, textSpeed/2);
     } else {
-        startButton.style.display='block';
+        document.querySelector('.containerStart').style.display='block';
     }
 }
 
 function showInstructions(){
-    startButton.style.cssText = 'text-align:center; margin-top:20px;'
-    instructionBtt.style.display = 'none';
     introductText.innerHTML = " ";
     instructionList();
-    document.querySelector('.elements').style.display = 'initial';
+    document.querySelector('.elements').style.display = 'grid';
 }
 
 
 function startGame(){
-    startButton.style.display = 'none';
-    instructionBtt.style.display = 'none';
+    buttonsContainers.forEach((buttons) => { buttons.style.display='none';})
     introductText.style.display= 'none';
     document.querySelector('.elements').style.display = 'none';
-    document.querySelector('.playButtons').style.display = 'initial';
+    document.querySelector('.playButtons').style.display = 'flex';
     document.querySelector('#pointsCounter').style.display= 'initial';
 }
 
@@ -71,10 +72,10 @@ function winner(){
         })
         if (playerScore > computerScore){
             statusMessage.innerHTML = 'You\'ve won'
-            statusMessage.style.cssText = 'animation-play-state: paused; color:green !important;';
+            statusMessage.style.cssText = 'animation-play-state: paused; color:green !important; font-size: 20px;';
         } else {
             statusMessage.innerHTML = 'You\'ve lost'
-            statusMessage.style.cssText = 'animation-play-state: paused; color:red !important;';
+            statusMessage.style.cssText = 'animation-play-state: paused; color:red !important;  font-size: 20px;';
         }  
     }
    
@@ -92,7 +93,7 @@ function battleStart(playerSelection, computerPlay){
     let roundWin = document.querySelector('.roundWinner');
     if (playerSelection === computerPlay){
                 playerChoice.innerHTML = `You\'ve chosen ${playerSelection} against`
-                computerChoice.innerHTML = `${computerPlay} spell`
+                computerChoice.innerHTML = `Duffy's ${computerPlay} spell`
                 roundWin.innerHTML = `That means a draw, you\'re both good magicians`
             }
             else if (playerSelection === options[0] && computerPlay !== options[1] || playerSelection === options[1] && computerPlay !== options[2] || playerSelection === options[2] && computerPlay !== options[0]){
@@ -119,10 +120,19 @@ function selections(){
         weapon.addEventListener('click', () => {
             if (weapon.classList.contains('fire_attack')){
                 playerSelection = options[0];
+                document.querySelector('.water_attack').style.borderColor='white';
+                document.querySelector('.fire_attack').style.borderColor='red';
+                document.querySelector('.grass_attack').style.borderColor='white';
             } else if ( weapon.classList.contains('water_attack')){
                 playerSelection = options[1];
+                document.querySelector('.water_attack').style.borderColor='blue';
+                document.querySelector('.fire_attack').style.borderColor='white';
+                document.querySelector('.grass_attack').style.borderColor='white';
             } else {
                 playerSelection = options[2];
+                document.querySelector('.water_attack').style.borderColor='white';
+                document.querySelector('.fire_attack').style.borderColor='white';
+                document.querySelector('.grass_attack').style.borderColor='green';
             }
             battleStart(playerSelection, computerSelection());
             winner();
